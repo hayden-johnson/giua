78ef8f1356c81b82565207c5ee128a425559df7c
hayden-johnson <joh18320@umn.edu>
2021-07-31 18:34:43 -0500

    stop rending templates

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, jsonify, json
 import parser
+import json
 
 app = Flask(__name__)
 
@@ -7,14 +8,28 @@ app = Flask(__name__)
 def index():
     return render_template("index.html") 
 
+
 @app.route("/parse", methods=["POST"])
 def parse(): 
-    url = request.form.get("url")
-    text=parser.parseText(url)
-    print(f"\nprinting text for [{url}]") 
-    print(text)
-    return render_template("calculated.html", text=text)
+    if request.content_type != 'application/json':
+        print('incorrect content type')
+        return jsonify({})
 
+    try:
+        print('loading json')
+        data = json.loads(request.data)
+    except:
+        print('failed to load json')
+        return jsonify({})
+
+    if 'url' not in data:
+        print('url not found in data')
+        return jsonify({})
+    url = data['url']
+    resp = {
+        'text': parser.parseText(url)
+    }
+    return jsonify(resp)
 
 
 if __name__ == "__main__":
