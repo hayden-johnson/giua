17543834315ae6981928ec84e48026ec55b6f0d0
Danielle Cruz <dcruz21@stanford.edu>
2021-08-01 13:39:28 -0500

    Updated title and description; removed unnecessary files

--- client/background.js
+++ client/background.js
@@ -1,6 +1,6 @@
-let color = '#3aa757';
+let languageLevel = '';
+let interests = [];
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.storage.sync.set({ languageLevel, interests });
 });
